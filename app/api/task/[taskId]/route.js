e5955aba137f96145079fbf28f77ea428c5241cb
huzaifah-dev <huzaifahkhan00@gmail.com>
2025-01-06 03:06:53 +0500

    add DELETE API endpoint for task management; implement task deletion logic and error handling

--- app/api/task/[taskId]/route.js
+++ app/api/task/[taskId]/route.js
@@ -34,3 +34,42 @@ export async function GET(request, { params }) {
     });
   }
 }
+
+export async function DELETE(request, { params }) {
+  await dbConnect();
+
+  const { taskId } = params;
+
+  if (!taskId) {
+    return new Response(JSON.stringify({ error: "Task ID is required" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    const task = await Task.findByIdAndDelete(taskId);
+    if (!task) {
+      return new Response(
+        JSON.stringify({ error: "Task not found or already deleted" }),
+        {
+          status: 404,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    return new Response(
+      JSON.stringify({ message: "Task successfully deleted" }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Error deleting task" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
